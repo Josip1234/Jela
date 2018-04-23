3923c1c27af607e1a214684c05172b49979eb725
Josip Bošnjak <jbosnjak@unipu.hr>
2018-04-23 10:38:42 +0200

    dodani testni kontroleri i middleware-ovi

---
+++ jela/app/Http/Controllers/ImplicitController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImplicitController extends Controller
+{
+    //
+	public function getIndex(){
+		echo 'index method';
+	}
+	
+}
